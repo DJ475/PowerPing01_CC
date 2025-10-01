7ca2678cb06067d508189d34aaa7f23b0b68f05e
Djibril <djibrilsylla475@gmail.com>
2025-09-30 21:20:37 -0600

    CSS Format Fixes,Forogt Password Clickable Link

--- src/UI_Components/Login_UI/LoginPage.tsx
+++ src/UI_Components/Login_UI/LoginPage.tsx
@@ -3,6 +3,8 @@ import { LoginBtnOBJ } from './LoginButton';
 import { LoginInputOBJ } from './InputField';
 import { RegisterBtnOBJ } from './RegisterButton';
 
+import styles from "./LoginPage.module.css"
+
 import React, { useState } from 'react';
 // import LoginPage from "./UI_Components/Login_UI/Login"
 // import ForgotPassword from "./UI_Components/ResetPassword_UI/ResetPswd"
@@ -12,11 +14,11 @@ export function LoginPage()
     const [username,setUsername] = useState("");
     const [passsword,setPassword] = useState("");
     return (
-        <div className="AppBody">
+        <div className={styles.ExampleBackground}>
         {/* <div className="NavBarContainer">
         </div> */}
         <header >
-        <h3 id="textGreeting"> <u>PowerPing01</u></h3>
+        <h3 id={styles.textGreeting}> <u>PowerPing01</u></h3>
         </header>
         
         {/* Call Login Function */}
@@ -40,15 +42,17 @@ export function LoginPage()
         <Route path="/" element={<LoginPage />}></Route>
         <Route path="forgotPassword" element={<ForgotPassword/>}/>
         </Routes> */}
-        {/* <div id="divForgotPswd">
-        <h3> <Link to="/forgotPassword"> Forgot Password? </Link></h3>
-        </div> */}
+
+        <div id={styles.divForgotPswd}>
+        <h3> Forgot Password? </h3>
+        </div>
         
         <ContainerBtn>
             <LoginBtnOBJ 
                 loginBtnText="Login"
                 onClickListenerL={(e)=>{alert("Logging In Now")}}
             />
+            
             
             <RegisterBtnOBJ
                 registerBtnText="Register"
